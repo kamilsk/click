46fac3871cf339aca9a2871ccfccec4a95d9cddf
Kamil Samigullin <kamil@samigullin.info>
2018-02-24 19:32:44 +0300

    fix #12: init project structure and create stub server

---
+++ server/router/chi/router.go
@@ -0,0 +1,31 @@
+package chi
+
+import (
+	"net/http"
+	"net/http/pprof"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func NewRouter(api interface{}, withProfiler bool) http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+
+	r.Get("/*", func(rw http.ResponseWriter, req *http.Request) { rw.WriteHeader(http.StatusNotImplemented) })
+
+	if withProfiler {
+		r.Route("/debug/pprof", func(r chi.Router) {
+			r.Get("/", pprof.Index)
+			r.Get("/cmdline", pprof.Cmdline)
+			r.Get("/profile", pprof.Profile)
+			r.Get("/symbol", pprof.Symbol)
+			r.Get("/trace", pprof.Trace)
+		})
+	}
+
+	return r
+}
