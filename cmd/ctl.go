da73f6d405b2b6eaf2df05f5be4f7f8121962d53
Kamil Samigullin <kamil@samigullin.info>
2018-07-25 22:01:47 +0300

    fix #101: configure access token

--- cmd/ctl.go
+++ cmd/ctl.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"log"
 
+	"github.com/kamilsk/click/pkg/config"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -14,6 +16,20 @@ var (
 )
 
 func init() {
+	var (
+		flags = controlCmd.PersistentFlags()
+		cnf   = config.GRPCConfig{}
+		v     = viper.New()
+	)
+	{
+		must(
+			func() error { return v.BindEnv("click_token") },
+		)
+		v.SetDefault("click_token", "")
+	}
+	{
+		flags.StringVarP((*string)(&cnf.Token), "token", "t", v.GetString("click_token"), "user access token")
+	}
 	controlCmd.AddCommand(
 		&cobra.Command{
 			Use:   "create",
