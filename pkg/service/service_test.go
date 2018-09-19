01f7fffc9d605e048f60d6331d29fae416473f20
Kamil Samigullin <kamil@samigullin.info>
2018-09-19 22:00:05 +0300

    review server/router

--- pkg/service/service_test.go
+++ pkg/service/service_test.go
@@ -1,8 +1,3 @@
 //go:generate echo $PWD/$GOPACKAGE/$GOFILE
 //go:generate mockgen -package service_test -destination $PWD/pkg/service/mock_contract_test.go github.com/kamilsk/click/pkg/service Storage
 package service_test
-
-import (
-	_ "github.com/golang/mock/gomock"
-	_ "github.com/golang/mock/mockgen/model"
-)
