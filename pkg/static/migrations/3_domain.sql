98c9a25b8630592d3151acbeff349448f3d5b2a0
Kamil Samigullin <kamil@samigullin.info>
2018-10-02 20:06:01 +0300

    reorder 3_domain.sql

--- pkg/static/migrations/3_domain.sql
+++ pkg/static/migrations/3_domain.sql
@@ -1,15 +1,5 @@
 -- +migrate Up
 
-CREATE TABLE "namespace" (
-  "id"         UUID        NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-  "account_id" UUID        NOT NULL,
-  "name"       VARCHAR(32) NOT NULL,
-  "created_at" TIMESTAMP   NOT NULL             DEFAULT now(),
-  "updated_at" TIMESTAMP   NULL                 DEFAULT NULL,
-  "deleted_at" TIMESTAMP   NULL                 DEFAULT NULL,
-  UNIQUE ("account_id", "name")
-);
-
 CREATE TABLE "link" (
   "id"         UUID         NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
   "account_id" UUID         NOT NULL,
@@ -18,18 +8,6 @@ CREATE TABLE "link" (
   "updated_at" TIMESTAMP    NULL                 DEFAULT NULL,
   "deleted_at" TIMESTAMP    NULL                 DEFAULT NULL,
   UNIQUE ("account_id", "name")
-);
-
-CREATE TABLE "alias" (
-  "id"           UUID         NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-  "account_id"   UUID         NOT NULL,
-  "link_id"      UUID         NOT NULL,
-  "namespace_id" UUID         NOT NULL,
-  "urn"          VARCHAR(512) NOT NULL,
-  "created_at"   TIMESTAMP    NOT NULL             DEFAULT now(),
-  "updated_at"   TIMESTAMP    NULL                 DEFAULT NULL,
-  "deleted_at"   TIMESTAMP    NULL                 DEFAULT NULL,
-  UNIQUE ("namespace_id", "urn")
 );
 
 CREATE TABLE "target" (
@@ -45,19 +23,31 @@ CREATE TABLE "target" (
   UNIQUE ("link_id", "uri")
 );
 
-CREATE TRIGGER "namespace_updated"
-  BEFORE UPDATE
-  ON "namespace"
-  FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
+CREATE TABLE "namespace" (
+  "id"         UUID        NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+  "account_id" UUID        NOT NULL,
+  "name"       VARCHAR(32) NOT NULL,
+  "created_at" TIMESTAMP   NOT NULL             DEFAULT now(),
+  "updated_at" TIMESTAMP   NULL                 DEFAULT NULL,
+  "deleted_at" TIMESTAMP   NULL                 DEFAULT NULL,
+  UNIQUE ("account_id", "name")
+);
+
+CREATE TABLE "alias" (
+  "id"           UUID         NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+  "account_id"   UUID         NOT NULL,
+  "link_id"      UUID         NOT NULL,
+  "namespace_id" UUID         NOT NULL,
+  "urn"          VARCHAR(512) NOT NULL,
+  "created_at"   TIMESTAMP    NOT NULL             DEFAULT now(),
+  "updated_at"   TIMESTAMP    NULL                 DEFAULT NULL,
+  "deleted_at"   TIMESTAMP    NULL                 DEFAULT NULL,
+  UNIQUE ("namespace_id", "urn")
+);
 
 CREATE TRIGGER "link_updated"
   BEFORE UPDATE
   ON "link"
-  FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
-
-CREATE TRIGGER "alias_updated"
-  BEFORE UPDATE
-  ON "alias"
   FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
 
 CREATE TRIGGER "target_updated"
@@ -65,26 +55,36 @@ CREATE TRIGGER "target_updated"
   ON "target"
   FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
 
+CREATE TRIGGER "namespace_updated"
+  BEFORE UPDATE
+  ON "namespace"
+  FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
+
+CREATE TRIGGER "alias_updated"
+  BEFORE UPDATE
+  ON "alias"
+  FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
+
 
 
 -- +migrate Down
 
-DROP TRIGGER "target_updated"
-ON "target";
-
 DROP TRIGGER "alias_updated"
 ON "alias";
-
-DROP TRIGGER "link_updated"
-ON "link";
 
 DROP TRIGGER "namespace_updated"
 ON "namespace";
 
-DROP TABLE "target";
+DROP TRIGGER "target_updated"
+ON "target";
+
+DROP TRIGGER "link_updated"
+ON "link";
 
 DROP TABLE "alias";
 
-DROP TABLE "link";
-
 DROP TABLE "namespace";
+
+DROP TABLE "target";
+
+DROP TABLE "link";
