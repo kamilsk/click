846c3ce25a35a5bd45a6a30b68dff6f10fe07aa9
Kamil Samigullin <kamil@samigullin.info>
2018-10-03 11:15:21 +0300

    issue #124: define protected storage methods

---
+++ pkg/storage/executor/internal/postgres/helper_test.go
@@ -0,0 +1,30 @@
+package postgres_test
+
+import (
+	"time"
+
+	"github.com/kamilsk/click/pkg/storage/types"
+)
+
+func DemoToken() *types.Token {
+	var (
+		token = types.Token{
+			ID:        "10000000-2000-4000-8000-160000000003",
+			UserID:    "10000000-2000-4000-8000-160000000002",
+			CreatedAt: time.Now(),
+		}
+		user = types.User{
+			AccountID: "10000000-2000-4000-8000-160000000001",
+			Name:      "Demo user",
+			CreatedAt: time.Now(),
+		}
+		account = types.Account{
+			Name:      "Demo account",
+			CreatedAt: time.Now(),
+		}
+	)
+	user.ID, account.ID = token.UserID, user.AccountID
+	token.User, user.Account = &user, &account
+	user.Tokens, account.Users = append(user.Tokens, &token), append(account.Users, &user)
+	return &token
+}
