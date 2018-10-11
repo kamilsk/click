c51de5c5365e400b1c6c6be1c6b660e6cac8d97a
Kamil Samigullin <kamil@samigullin.info>
2018-10-11 18:23:37 +0300

    issue #124: fix service layer

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,49 +1,62 @@
 package service
 
 import (
-	"github.com/kamilsk/click/pkg/domain"
+	"context"
+
 	"github.com/kamilsk/click/pkg/transfer"
 	"github.com/kamilsk/click/pkg/transfer/api/v1"
 )
 
 // New returns a new instance of Click! service.
-func New(dao Storage) *Click {
-	return &Click{dao: dao}
+func New(storage Storage, handler RedirectHandler) *Click {
+	return &Click{storage, handler}
 }
 
 // Click is the primary application service.
 type Click struct {
-	dao Storage
+	storage Storage
+	handler RedirectHandler
 }
 
 // HandleGetV1 handles an input request.
-func (s *Click) HandleGetV1(request v1.GetRequest) v1.GetResponse {
+// Deprecated TODO issue#version3.0 use LinkEditor and gRPC gateway instead
+func (service *Click) HandleGetV1(ctx context.Context, request v1.GetRequest) v1.GetResponse {
 	var response v1.GetResponse
-	response.Link, response.Error = s.dao.Link(request.ID)
+	response.Link, response.Error = service.storage.Link(ctx, request.ID)
 	return response
 }
 
 // HandlePass handles an input request.
-func (s *Click) HandlePass(request transfer.PassRequest) transfer.PassResponse {
+func (service *Click) HandlePass(ctx context.Context, request transfer.PassRequest) transfer.PassResponse {
 	var response transfer.PassResponse
+
+	// TODO issue#51
+	if !request.Option.NoLog {
+		// if request.Option.Anonymously {}
+		request.Redirect.Identifier = "10000000-2000-4000-8000-160000000000" // TODO issue#134
+		_ = service.handler.LogRedirect(ctx, request.Redirect)
+	}
+
 	return response
 }
 
 // HandleRedirect handles an input request.
-func (s *Click) HandleRedirect(request transfer.RedirectRequest) transfer.RedirectResponse {
+func (service *Click) HandleRedirect(ctx context.Context, request transfer.RedirectRequest) transfer.RedirectResponse {
 	var response transfer.RedirectResponse
-	link, err := s.dao.LinkByAlias(request.Namespace, request.URN)
+	link, err := service.storage.LinkByAlias(ctx, request.Namespace, request.URN)
 	if err != nil {
 		response.Error = err
 		return response
 	}
 	response.Alias = link.Aliases.Find(request.Namespace, request.URN)
 	response.Target = link.Targets.Find(response.Alias, request.Query)
-	response.Alias.LinkID, response.Target.LinkID = link.ID, link.ID
-	return response
-}
 
-// LogRedirectEvent stores a "redirect event".
-func (s *Click) LogRedirectEvent(event domain.Log) {
-	_, _ = s.dao.Log(event)
+	// TODO issue#51
+	if !request.Option.NoLog {
+		// if request.Option.Anonymously {}
+		request.Redirect.Identifier = "10000000-2000-4000-8000-160000000000" // TODO issue#134
+		_ = service.handler.LogRedirect(ctx, request.Redirect)
+	}
+
+	return response
 }
