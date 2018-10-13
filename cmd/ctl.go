f81acc247ffad1933786371a5f0ec45f774edc23
Kamil Samigullin <kamil@samigullin.info>
2018-10-13 18:02:31 +0300

    issue #125: cover database layer

--- cmd/ctl.go
+++ cmd/ctl.go
@@ -1,25 +1,14 @@
 package cmd
 
 import (
-	"context"
-	"encoding/json"
-	"io"
-	"io/ioutil"
-	"os"
-	"reflect"
-	"strings"
+	"fmt"
+	"strconv"
 	"time"
 
-	pb "github.com/kamilsk/click/pkg/server/grpc"
-
-	"github.com/kamilsk/click/pkg/config"
 	"github.com/kamilsk/go-kit/pkg/fn"
-	"github.com/mitchellh/mapstructure"
-	"github.com/pkg/errors"
+	"github.com/kamilsk/go-kit/pkg/strings"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"google.golang.org/grpc"
-	"gopkg.in/yaml.v2"
 )
 
 const (
@@ -27,204 +16,34 @@ const (
 	jsonFormat = "json"
 )
 
-type schema struct {
-	Kind    string                 `yaml:"kind"`
-	Payload map[string]interface{} `yaml:"payload"`
-}
-
-type factory map[*cobra.Command]map[string]func() interface{}
-
-func (f factory) new(cmd *cobra.Command) (interface{}, error) {
-	data, err := f.data(cmd.Flag("filename").Value.String())
-	if err != nil {
-		return nil, err
-	}
-	builder, ok := f[cmd][data.Kind]
-	if !ok {
-		return nil, errors.Errorf("unknown payload type %q", data.Kind)
-	}
-	entity := builder()
-	if err = mapstructure.Decode(data.Payload, &entity); err != nil {
-		return nil, errors.Wrapf(err, "trying to decode payload to %#v", entity)
-	}
-	return entity, nil
-}
-
-func (f factory) data(name string) (schema, error) {
-	var (
-		err error
-		out schema
-		raw []byte
-		src io.Reader = os.Stdin
-	)
-	if name != "" {
-		if src, err = os.Open(name); err != nil {
-			return out, errors.Wrapf(err, "trying to open file %q", name)
-		}
-	} else {
-		name = "/dev/stdin"
-	}
-	if raw, err = ioutil.ReadAll(src); err != nil {
-		return out, errors.Wrapf(err, "trying to read file %q", name)
-	}
-	err = yaml.Unmarshal(raw, &out)
-	return out, errors.Wrapf(err, "trying to decode file %q as YAML", name)
-}
-
-func call(cnf config.GRPCConfig, entity interface{}) (interface{}, error) {
-	conn, err := grpc.Dial(cnf.Interface, grpc.WithInsecure())
-	if err != nil {
-		return nil, errors.Wrapf(err, "trying to connect to the gRPC server %q", cnf.Interface)
-	}
-	defer conn.Close()
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	switch request := entity.(type) {
-	case *pb.CreateNamespaceRequest:
-		client := pb.NewNamespaceClient(conn)
-		return client.Create(ctx, request)
-	case *pb.CreateLinkRequest:
-		client := pb.NewLinkClient(conn)
-		return client.Create(ctx, request)
-	case *pb.CreateAliasRequest:
-		client := pb.NewAliasClient(conn)
-		return client.Create(ctx, request)
-	case *pb.CreateTargetRequest:
-		client := pb.NewTargetClient(conn)
-		return client.Create(ctx, request)
-	case *pb.ReadNamespaceRequest:
-		client := pb.NewNamespaceClient(conn)
-		return client.Read(ctx, request)
-	case *pb.ReadLinkRequest:
-		client := pb.NewLinkClient(conn)
-		return client.Read(ctx, request)
-	case *pb.ReadAliasRequest:
-		client := pb.NewAliasClient(conn)
-		return client.Read(ctx, request)
-	case *pb.ReadTargetRequest:
-		client := pb.NewTargetClient(conn)
-		return client.Read(ctx, request)
-	case *pb.UpdateNamespaceRequest:
-		client := pb.NewNamespaceClient(conn)
-		return client.Update(ctx, request)
-	case *pb.UpdateLinkRequest:
-		client := pb.NewLinkClient(conn)
-		return client.Update(ctx, request)
-	case *pb.UpdateAliasRequest:
-		client := pb.NewAliasClient(conn)
-		return client.Update(ctx, request)
-	case *pb.UpdateTargetRequest:
-		client := pb.NewTargetClient(conn)
-		return client.Update(ctx, request)
-	case *pb.DeleteNamespaceRequest:
-		client := pb.NewNamespaceClient(conn)
-		return client.Delete(ctx, request)
-	case *pb.DeleteLinkRequest:
-		client := pb.NewLinkClient(conn)
-		return client.Delete(ctx, request)
-	case *pb.DeleteAliasRequest:
-		client := pb.NewAliasClient(conn)
-		return client.Delete(ctx, request)
-	case *pb.DeleteTargetRequest:
-		client := pb.NewTargetClient(conn)
-		return client.Delete(ctx, request)
-	default:
-		return nil, errors.Errorf("unknown type %T", request)
-	}
-}
-
-func convert(entity interface{}) map[string]interface{} {
-	t := reflect.ValueOf(entity).Elem().Type()
-	m := make(map[string]interface{}, t.NumField())
-	for i := 0; i < t.NumField(); i++ {
-		f := t.Field(i)
-		v, ok := f.Tag.Lookup("json")
-		if !ok {
-			continue
-		}
-		p := strings.Split(v, ",")
-		if p[0] == "-" {
-			continue
-		}
-		switch f.Type.String() {
-		case "[]uint8":
-			m[p[0]] = "binary"
-		default:
-			m[p[0]] = f.Type.String()
-		}
-	}
-	return m
-}
-
-func edit(cmd *cobra.Command, _ []string) error {
-	entity, err := entities.new(cmd)
-	if err != nil {
-		return err
-	}
-	if dry, _ := cmd.Flags().GetBool("dry-run"); dry {
-		cmd.Printf("%T would be sent with data: ", entity)
-		return json.NewEncoder(cmd.OutOrStdout()).Encode(entity)
-	}
-	response, err := call(cnf.Union.GRPCConfig, entity)
-	if err != nil {
-		return err
-	}
-	if cmd.Flag("output").Value.String() == jsonFormat {
-		return json.NewEncoder(cmd.OutOrStdout()).Encode(response)
-	}
-	return yaml.NewEncoder(cmd.OutOrStdout()).Encode(response)
-}
-
-var entities factory
-
 var (
-	controlCmd = &cobra.Command{Use: "ctl", Short: "Communicate with Click! server via gRPC"}
-	createCmd  = &cobra.Command{Use: "create", Short: "Create some kind", RunE: edit}
-	readCmd    = &cobra.Command{Use: "read", Short: "Read some kind", RunE: edit}
-	updateCmd  = &cobra.Command{Use: "update", Short: "Update some kind", RunE: edit}
-	deleteCmd  = &cobra.Command{Use: "delete", Short: "Delete some kind", RunE: edit}
-	schemaCmd  = &cobra.Command{
-		Use:   "schema",
-		Short: "Print schema of another control command",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			var (
-				target   *cobra.Command
-				builders map[string]func() interface{}
-				found    bool
-			)
-			use := cmd.Flag("for").Value.String()
-			for target, builders = range entities {
-				if strings.EqualFold(target.Use, use) {
-					found = true
-					break
-				}
-			}
-			if !found {
-				return errors.Errorf("unknown control command %q", use)
-			}
-			for kind, builder := range builders {
-				yaml.NewEncoder(cmd.OutOrStdout()).Encode(schema{Kind: kind, Payload: convert(builder())})
-				cmd.Println()
-			}
-			return nil
-		},
-	}
+	controlCmd = &cobra.Command{Use: "ctl", Short: "Click! Service Control"}
+	createCmd  = &cobra.Command{Use: "create", Short: "Create some kind", RunE: communicate}
+	readCmd    = &cobra.Command{Use: "read", Short: "Read some kind", RunE: communicate}
+	updateCmd  = &cobra.Command{Use: "update", Short: "Update some kind", RunE: communicate}
+	deleteCmd  = &cobra.Command{Use: "delete", Short: "Delete some kind", RunE: communicate}
+	schemaCmd  = &cobra.Command{Use: "schema", Short: "Print schemas of another control command", RunE: printSchemas}
 )
 
 func init() {
 	v := viper.New()
 	fn.Must(
+		func() error { return v.BindEnv("bind") },
+		func() error { return v.BindEnv("grpc_port") },
 		func() error { return v.BindEnv("click_token") },
-		func() error { return v.BindEnv("grpc_host") },
 		func() error {
-			v.SetDefault("click_token", defaults["click_token"])
-			v.SetDefault("grpc_host", defaults["grpc_host"])
+			v.SetDefault("bind", defaults["bind"])
+			v.SetDefault("grpc_port", defaults["grpc_port"])
+			v.SetDefault("grpc_host", strings.Concat(v.GetString("bind"), ":", strconv.Itoa(v.GetInt("grpc_port"))))
+			v.SetDefault("forma_token", defaults["click_token"])
 			return nil
 		},
 		func() error {
 			flags := controlCmd.PersistentFlags()
 			flags.StringVarP(new(string), "filename", "f", "", "entity source (default is standard input)")
-			flags.StringVarP(new(string), "output", "o", yamlFormat, "output format, one of: json|yaml")
+			flags.StringVarP(new(string), "output", "o", yamlFormat, fmt.Sprintf(
+				"output format, one of: %s|%s",
+				jsonFormat, yamlFormat))
 			flags.Bool("dry-run", false, "if true, only print the object that would be sent, without sending it")
 			flags.StringVarP(&cnf.Union.GRPCConfig.Interface,
 				"grpc-host", "", v.GetString("grpc_host"), "gRPC server host")
@@ -234,35 +53,6 @@ func init() {
 				"token", "", v.GetString("click_token"), "user access token")
 			schemaCmd.Flags().String("for", "", "which command: create, read, update or delete")
 			return schemaCmd.MarkFlagRequired("for")
-		},
-		func() error {
-			entities = factory{
-				createCmd: {
-					"Namespace": func() interface{} { return &pb.CreateNamespaceRequest{} },
-					"Link":      func() interface{} { return &pb.CreateLinkRequest{} },
-					"Alias":     func() interface{} { return &pb.CreateAliasRequest{} },
-					"Target":    func() interface{} { return &pb.CreateTargetRequest{} },
-				},
-				readCmd: {
-					"Namespace": func() interface{} { return &pb.ReadNamespaceRequest{} },
-					"Link":      func() interface{} { return &pb.ReadLinkRequest{} },
-					"Alias":     func() interface{} { return &pb.ReadAliasRequest{} },
-					"Target":    func() interface{} { return &pb.ReadTargetRequest{} },
-				},
-				updateCmd: {
-					"Namespace": func() interface{} { return &pb.UpdateNamespaceRequest{} },
-					"Link":      func() interface{} { return &pb.UpdateLinkRequest{} },
-					"Alias":     func() interface{} { return &pb.UpdateAliasRequest{} },
-					"Target":    func() interface{} { return &pb.UpdateTargetRequest{} },
-				},
-				deleteCmd: {
-					"Namespace": func() interface{} { return &pb.DeleteNamespaceRequest{} },
-					"Link":      func() interface{} { return &pb.DeleteLinkRequest{} },
-					"Alias":     func() interface{} { return &pb.DeleteAliasRequest{} },
-					"Target":    func() interface{} { return &pb.DeleteTargetRequest{} },
-				},
-			}
-			return nil
 		},
 	)
 	controlCmd.AddCommand(createCmd, readCmd, updateCmd, deleteCmd, schemaCmd)
--- cmd/grpc_helper.go
+++ cmd/grpc_helper.go
@@ -1,1 +1,232 @@
 package cmd
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"strings"
+
+	pb "github.com/kamilsk/click/pkg/server/grpc"
+	kit "github.com/kamilsk/go-kit/pkg/strings"
+
+	"github.com/kamilsk/click/pkg/config"
+	"github.com/kamilsk/click/pkg/server/grpc/middleware"
+	"github.com/mitchellh/mapstructure"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	linkKind      kind = "Link"
+	namespaceKind kind = "Namespace"
+	aliasKind     kind = "Alias"
+	targetKind    kind = "Target"
+)
+
+var entities factory
+
+func init() {
+	entities = factory{
+		createCmd: {
+			linkKind:      func() interface{} { return &pb.CreateLinkRequest{} },
+			namespaceKind: func() interface{} { return &pb.CreateNamespaceRequest{} },
+			aliasKind:     func() interface{} { return &pb.CreateAliasRequest{} },
+			targetKind:    func() interface{} { return &pb.CreateTargetRequest{} },
+		},
+		readCmd: {
+			linkKind:      func() interface{} { return &pb.ReadLinkRequest{} },
+			namespaceKind: func() interface{} { return &pb.ReadNamespaceRequest{} },
+			aliasKind:     func() interface{} { return &pb.ReadAliasRequest{} },
+			targetKind:    func() interface{} { return &pb.ReadTargetRequest{} },
+		},
+		updateCmd: {
+			linkKind:      func() interface{} { return &pb.UpdateLinkRequest{} },
+			namespaceKind: func() interface{} { return &pb.UpdateNamespaceRequest{} },
+			aliasKind:     func() interface{} { return &pb.UpdateAliasRequest{} },
+			targetKind:    func() interface{} { return &pb.UpdateTargetRequest{} },
+		},
+		deleteCmd: {
+			linkKind:      func() interface{} { return &pb.DeleteLinkRequest{} },
+			namespaceKind: func() interface{} { return &pb.DeleteNamespaceRequest{} },
+			aliasKind:     func() interface{} { return &pb.DeleteAliasRequest{} },
+			targetKind:    func() interface{} { return &pb.DeleteTargetRequest{} },
+		},
+	}
+}
+
+func communicate(cmd *cobra.Command, _ []string) error {
+	entity, err := entities.new(cmd)
+	if err != nil {
+		return err
+	}
+	if dry, _ := cmd.Flags().GetBool("dry-run"); dry {
+		cmd.Printf("%T would be sent with data: ", entity)
+		if cmd.Flag("output").Value.String() == jsonFormat {
+			return json.NewEncoder(cmd.OutOrStdout()).Encode(entity)
+		}
+		return json.NewEncoder(cmd.OutOrStdout()).Encode(entity)
+	}
+	response, err := call(cnf.Union.GRPCConfig, entity)
+	if err != nil {
+		cmd.Println(err)
+		return nil
+	}
+	if cmd.Flag("output").Value.String() == jsonFormat {
+		return json.NewEncoder(cmd.OutOrStdout()).Encode(response)
+	}
+	return yaml.NewEncoder(cmd.OutOrStdout()).Encode(response)
+}
+
+func printSchemas(cmd *cobra.Command, _ []string) error {
+	var (
+		target   *cobra.Command
+		builders map[kind]builder
+		found    bool
+	)
+	use := cmd.Flag("for").Value.String()
+	for target, builders = range entities {
+		if strings.EqualFold(target.Use, use) {
+			found = true
+			break
+		}
+	}
+	if !found {
+		return errors.Errorf("unknown control command %q", use)
+	}
+	for k, b := range builders {
+		_ = yaml.NewEncoder(cmd.OutOrStdout()).Encode(schema{Kind: k, Payload: convert(b())})
+		cmd.Println()
+	}
+	return nil
+}
+
+type builder func() interface{}
+
+type kind string
+
+type schema struct {
+	Kind    kind                   `yaml:"kind"`
+	Payload map[string]interface{} `yaml:"payload"`
+}
+
+type factory map[*cobra.Command]map[kind]builder
+
+func (f factory) new(cmd *cobra.Command) (interface{}, error) {
+	data, err := f.data(cmd.Flag("filename").Value.String())
+	if err != nil {
+		return nil, err
+	}
+	build, found := f[cmd][data.Kind]
+	if !found {
+		return nil, errors.Errorf("unknown payload type %q", data.Kind)
+	}
+	entity := build()
+	if err = mapstructure.Decode(data.Payload, &entity); err != nil {
+		return nil, errors.Wrapf(err, "trying to decode payload to %#v", entity)
+	}
+	return entity, nil
+}
+
+func (f factory) data(name string) (schema, error) {
+	var (
+		err error
+		out schema
+		raw []byte
+		src io.Reader = os.Stdin
+	)
+	if name != "" {
+		if src, err = os.Open(name); err != nil {
+			return out, errors.Wrapf(err, "trying to open file %q", name)
+		}
+	} else {
+		name = "/dev/stdin"
+	}
+	if raw, err = ioutil.ReadAll(src); err != nil {
+		return out, errors.Wrapf(err, "trying to read file %q", name)
+	}
+	err = yaml.Unmarshal(raw, &out)
+	return out, errors.Wrapf(err, "trying to decode file %q as YAML", name)
+}
+
+func call(cnf config.GRPCConfig, entity interface{}) (interface{}, error) {
+	conn, err := grpc.Dial(cnf.Interface, grpc.WithInsecure())
+	if err != nil {
+		return nil, errors.Wrapf(err, "trying to connect to the gRPC server %q", cnf.Interface)
+	}
+	defer conn.Close()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ctx = metadata.AppendToOutgoingContext(ctx,
+		middleware.AuthHeader,
+		kit.Concat(middleware.AuthScheme, " ", string(cnf.Token)))
+	switch request := entity.(type) {
+
+	case *pb.CreateLinkRequest:
+		return pb.NewLinkClient(conn).Create(ctx, request)
+	case *pb.CreateNamespaceRequest:
+		return pb.NewNamespaceClient(conn).Create(ctx, request)
+	case *pb.CreateAliasRequest:
+		return pb.NewAliasClient(conn).Create(ctx, request)
+	case *pb.CreateTargetRequest:
+		return pb.NewTargetClient(conn).Create(ctx, request)
+
+	case *pb.ReadLinkRequest:
+		return pb.NewLinkClient(conn).Read(ctx, request)
+	case *pb.ReadNamespaceRequest:
+		return pb.NewNamespaceClient(conn).Read(ctx, request)
+	case *pb.ReadAliasRequest:
+		return pb.NewAliasClient(conn).Read(ctx, request)
+	case *pb.ReadTargetRequest:
+		return pb.NewTargetClient(conn).Read(ctx, request)
+
+	case *pb.UpdateLinkRequest:
+		return pb.NewLinkClient(conn).Update(ctx, request)
+	case *pb.UpdateNamespaceRequest:
+		return pb.NewNamespaceClient(conn).Update(ctx, request)
+	case *pb.UpdateAliasRequest:
+		return pb.NewAliasClient(conn).Update(ctx, request)
+	case *pb.UpdateTargetRequest:
+		return pb.NewTargetClient(conn).Update(ctx, request)
+
+	case *pb.DeleteLinkRequest:
+		return pb.NewLinkClient(conn).Delete(ctx, request)
+	case *pb.DeleteNamespaceRequest:
+		return pb.NewNamespaceClient(conn).Delete(ctx, request)
+	case *pb.DeleteAliasRequest:
+		return pb.NewAliasClient(conn).Delete(ctx, request)
+	case *pb.DeleteTargetRequest:
+		return pb.NewTargetClient(conn).Delete(ctx, request)
+
+	default:
+		return nil, errors.Errorf("unknown type %T", request)
+	}
+}
+
+func convert(entity interface{}) map[string]interface{} {
+	t := reflect.ValueOf(entity).Elem().Type()
+	m := make(map[string]interface{}, t.NumField())
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		v, ok := f.Tag.Lookup("json")
+		if !ok {
+			continue
+		}
+		p := strings.Split(v, ",")
+		if p[0] == "-" {
+			continue
+		}
+		switch f.Type.String() {
+		case "[]uint8":
+			m[p[0]] = "binary"
+		default:
+			m[p[0]] = f.Type.String()
+		}
+	}
+	return m
+}
