903efdf9a4bb3cf197fa423807345901e2161244
Kamil Samigullin <kamil@samigullin.info>
2018-10-14 14:46:01 +0300

    issue #125: implement namespace server

--- pkg/server/grpc/server_namespace.go
+++ pkg/server/grpc/server_namespace.go
@@ -2,7 +2,12 @@ package grpc
 
 import (
 	"context"
-	"log"
+
+	"github.com/kamilsk/click/pkg/domain"
+	"github.com/kamilsk/click/pkg/server/grpc/middleware"
+	"github.com/kamilsk/click/pkg/storage/query"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // NewNamespaceServer returns new instance of server API for Namespace service.
@@ -15,25 +20,74 @@ type namespaceServer struct {
 }
 
 // Create TODO issue#131
-func (*namespaceServer) Create(context.Context, *CreateNamespaceRequest) (*CreateNamespaceResponse, error) {
-	log.Println("NamespaceServer.Create was called")
-	return &CreateNamespaceResponse{}, nil
+func (server *namespaceServer) Create(ctx context.Context, req *CreateNamespaceRequest) (*CreateNamespaceResponse, error) {
+	token, authErr := middleware.TokenExtractor(ctx)
+	if authErr != nil {
+		return nil, authErr
+	}
+	namespace, createErr := server.storage.CreateNamespace(ctx, token, query.CreateNamespace{
+		ID:   ptrToID(req.Id),
+		Name: req.Name,
+	})
+	if createErr != nil {
+		return nil, status.Errorf(codes.Internal, "error happened: %+v", createErr)
+	}
+	return &CreateNamespaceResponse{
+		Id:        namespace.ID.String(),
+		CreatedAt: Timestamp(&namespace.CreatedAt),
+	}, nil
 }
 
 // Read TODO issue#131
-func (*namespaceServer) Read(context.Context, *ReadNamespaceRequest) (*ReadNamespaceResponse, error) {
-	log.Println("NamespaceServer.Read was called")
-	return &ReadNamespaceResponse{}, nil
+func (server *namespaceServer) Read(ctx context.Context, req *ReadNamespaceRequest) (*ReadNamespaceResponse, error) {
+	token, authErr := middleware.TokenExtractor(ctx)
+	if authErr != nil {
+		return nil, authErr
+	}
+	namespace, readErr := server.storage.ReadNamespace(ctx, token, query.ReadNamespace{ID: domain.ID(req.Id)})
+	if readErr != nil {
+		return nil, status.Errorf(codes.Internal, "error happened: %+v", readErr)
+	}
+	return &ReadNamespaceResponse{
+		Id:        namespace.ID.String(),
+		Name:      namespace.Name,
+		CreatedAt: Timestamp(&namespace.CreatedAt),
+		UpdatedAt: Timestamp(namespace.UpdatedAt),
+		DeletedAt: Timestamp(namespace.DeletedAt),
+	}, nil
 }
 
 // Update TODO issue#131
-func (*namespaceServer) Update(context.Context, *UpdateNamespaceRequest) (*UpdateNamespaceResponse, error) {
-	log.Println("NamespaceServer.Update was called")
-	return &UpdateNamespaceResponse{}, nil
+func (server *namespaceServer) Update(ctx context.Context, req *UpdateNamespaceRequest) (*UpdateNamespaceResponse, error) {
+	token, authErr := middleware.TokenExtractor(ctx)
+	if authErr != nil {
+		return nil, authErr
+	}
+	namespace, updateErr := server.storage.UpdateNamespace(ctx, token, query.UpdateNamespace{
+		ID:   domain.ID(req.Id),
+		Name: req.Name,
+	})
+	if updateErr != nil {
+		return nil, status.Errorf(codes.Internal, "error happened: %+v", updateErr)
+	}
+	return &UpdateNamespaceResponse{
+		Id:        namespace.ID.String(),
+		UpdatedAt: Timestamp(namespace.UpdatedAt),
+	}, nil
 }
 
 // Delete TODO issue#131
-func (*namespaceServer) Delete(context.Context, *DeleteNamespaceRequest) (*DeleteNamespaceResponse, error) {
-	log.Println("NamespaceServer.Delete was called")
-	return &DeleteNamespaceResponse{}, nil
+func (server *namespaceServer) Delete(ctx context.Context, req *DeleteNamespaceRequest) (*DeleteNamespaceResponse, error) {
+	token, authErr := middleware.TokenExtractor(ctx)
+	if authErr != nil {
+		return nil, authErr
+	}
+	namespace, deleteErr := server.storage.DeleteNamespace(ctx, token, query.DeleteNamespace{ID: domain.ID(req.Id)})
+	if deleteErr != nil {
+		return nil, status.Errorf(codes.Internal, "error happened: %+v", deleteErr)
+	}
+	return &DeleteNamespaceResponse{
+		Id:        namespace.ID.String(),
+		DeletedAt: Timestamp(namespace.DeletedAt),
+	}, nil
 }
