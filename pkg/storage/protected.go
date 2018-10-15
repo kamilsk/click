9b96f2d8dabca8f9558736873a072f4b3696cd3c
Kamil Samigullin <kamil@samigullin.info>
2018-10-15 10:11:20 +0300

    check link and namespace availability before creating alias and target

--- pkg/storage/protected.go
+++ pkg/storage/protected.go
@@ -6,6 +6,7 @@ import (
 	"github.com/kamilsk/click/pkg/domain"
 	"github.com/kamilsk/click/pkg/storage/query"
 	"github.com/kamilsk/click/pkg/storage/types"
+	"golang.org/x/sync/errgroup"
 )
 
 /*
@@ -18,15 +19,15 @@ import (
 func (storage *Storage) CreateLink(ctx context.Context, tokenID domain.ID, data query.CreateLink) (types.Link, error) {
 	var entity types.Link
 
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
 	}
 	defer closer()
 
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
 	}
 	return storage.exec.LinkEditor(ctx, conn).Create(token, data)
 }
@@ -35,15 +36,15 @@ func (storage *Storage) CreateLink(ctx context.Context, tokenID domain.ID, data 
 func (storage *Storage) ReadLink(ctx context.Context, tokenID domain.ID, data query.ReadLink) (types.Link, error) {
 	var entity types.Link
 
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
 	}
 	defer closer()
 
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
 	}
 	return storage.exec.LinkEditor(ctx, conn).Read(token, data)
 }
@@ -52,15 +53,15 @@ func (storage *Storage) ReadLink(ctx context.Context, tokenID domain.ID, data qu
 func (storage *Storage) UpdateLink(ctx context.Context, tokenID domain.ID, data query.UpdateLink) (types.Link, error) {
 	var entity types.Link
 
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
 	}
 	defer closer()
 
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
 	}
 	return storage.exec.LinkEditor(ctx, conn).Update(token, data)
 }
@@ -69,91 +70,17 @@ func (storage *Storage) UpdateLink(ctx context.Context, tokenID domain.ID, data 
 func (storage *Storage) DeleteLink(ctx context.Context, tokenID domain.ID, data query.DeleteLink) (types.Link, error) {
 	var entity types.Link
 
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
 	}
 	defer closer()
 
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
 	}
 	return storage.exec.LinkEditor(ctx, conn).Delete(token, data)
-}
-
-/*
- *
- * Target
- *
- */
-
-// CreateTarget TODO issue#131
-func (storage *Storage) CreateTarget(ctx context.Context, tokenID domain.ID, data query.CreateTarget) (types.Target, error) {
-	var entity types.Target
-
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
-	}
-	defer closer()
-
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
-	}
-	return storage.exec.TargetEditor(ctx, conn).Create(token, data)
-}
-
-// ReadTarget TODO issue#131
-func (storage *Storage) ReadTarget(ctx context.Context, tokenID domain.ID, data query.ReadTarget) (types.Target, error) {
-	var entity types.Target
-
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
-	}
-	defer closer()
-
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
-	}
-	return storage.exec.TargetEditor(ctx, conn).Read(token, data)
-}
-
-// UpdateTarget TODO issue#131
-func (storage *Storage) UpdateTarget(ctx context.Context, tokenID domain.ID, data query.UpdateTarget) (types.Target, error) {
-	var entity types.Target
-
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
-	}
-	defer closer()
-
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
-	}
-	return storage.exec.TargetEditor(ctx, conn).Update(token, data)
-}
-
-// DeleteTarget TODO issue#131
-func (storage *Storage) DeleteTarget(ctx context.Context, tokenID domain.ID, data query.DeleteTarget) (types.Target, error) {
-	var entity types.Target
-
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
-	}
-	defer closer()
-
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
-	}
-	return storage.exec.TargetEditor(ctx, conn).Delete(token, data)
 }
 
 /*
@@ -166,15 +93,15 @@ func (storage *Storage) DeleteTarget(ctx context.Context, tokenID domain.ID, dat
 func (storage *Storage) CreateNamespace(ctx context.Context, tokenID domain.ID, data query.CreateNamespace) (types.Namespace, error) {
 	var entity types.Namespace
 
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
 	}
 	defer closer()
 
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
 	}
 	return storage.exec.NamespaceEditor(ctx, conn).Create(token, data)
 }
@@ -183,15 +110,15 @@ func (storage *Storage) CreateNamespace(ctx context.Context, tokenID domain.ID, 
 func (storage *Storage) ReadNamespace(ctx context.Context, tokenID domain.ID, data query.ReadNamespace) (types.Namespace, error) {
 	var entity types.Namespace
 
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
 	}
 	defer closer()
 
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
 	}
 	return storage.exec.NamespaceEditor(ctx, conn).Read(token, data)
 }
@@ -200,15 +127,15 @@ func (storage *Storage) ReadNamespace(ctx context.Context, tokenID domain.ID, da
 func (storage *Storage) UpdateNamespace(ctx context.Context, tokenID domain.ID, data query.UpdateNamespace) (types.Namespace, error) {
 	var entity types.Namespace
 
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
 	}
 	defer closer()
 
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
 	}
 	return storage.exec.NamespaceEditor(ctx, conn).Update(token, data)
 }
@@ -217,15 +144,15 @@ func (storage *Storage) UpdateNamespace(ctx context.Context, tokenID domain.ID, 
 func (storage *Storage) DeleteNamespace(ctx context.Context, tokenID domain.ID, data query.DeleteNamespace) (types.Namespace, error) {
 	var entity types.Namespace
 
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
 	}
 	defer closer()
 
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
 	}
 	return storage.exec.NamespaceEditor(ctx, conn).Delete(token, data)
 }
@@ -240,15 +167,27 @@ func (storage *Storage) DeleteNamespace(ctx context.Context, tokenID domain.ID, 
 func (storage *Storage) CreateAlias(ctx context.Context, tokenID domain.ID, data query.CreateAlias) (types.Alias, error) {
 	var entity types.Alias
 
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
 	}
 	defer closer()
 
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
+	}
+	g := &errgroup.Group{}
+	g.Go(func() error {
+		_, readErr := storage.exec.LinkEditor(ctx, conn).Read(token, query.ReadLink{ID: data.LinkID})
+		return readErr
+	})
+	g.Go(func() error {
+		_, readErr := storage.exec.NamespaceEditor(ctx, conn).Read(token, query.ReadNamespace{ID: data.NamespaceID})
+		return readErr
+	})
+	if readErr := g.Wait(); readErr != nil {
+		return entity, readErr
 	}
 	return storage.exec.AliasEditor(ctx, conn).Create(token, data)
 }
@@ -257,15 +196,15 @@ func (storage *Storage) CreateAlias(ctx context.Context, tokenID domain.ID, data
 func (storage *Storage) ReadAlias(ctx context.Context, tokenID domain.ID, data query.ReadAlias) (types.Alias, error) {
 	var entity types.Alias
 
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
 	}
 	defer closer()
 
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
 	}
 	return storage.exec.AliasEditor(ctx, conn).Read(token, data)
 }
@@ -274,15 +213,27 @@ func (storage *Storage) ReadAlias(ctx context.Context, tokenID domain.ID, data q
 func (storage *Storage) UpdateAlias(ctx context.Context, tokenID domain.ID, data query.UpdateAlias) (types.Alias, error) {
 	var entity types.Alias
 
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
 	}
 	defer closer()
 
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
+	}
+	g := &errgroup.Group{}
+	g.Go(func() error {
+		_, readErr := storage.exec.LinkEditor(ctx, conn).Read(token, query.ReadLink{ID: data.LinkID})
+		return readErr
+	})
+	g.Go(func() error {
+		_, readErr := storage.exec.NamespaceEditor(ctx, conn).Read(token, query.ReadNamespace{ID: data.NamespaceID})
+		return readErr
+	})
+	if readErr := g.Wait(); readErr != nil {
+		return entity, readErr
 	}
 	return storage.exec.AliasEditor(ctx, conn).Update(token, data)
 }
@@ -291,15 +242,95 @@ func (storage *Storage) UpdateAlias(ctx context.Context, tokenID domain.ID, data
 func (storage *Storage) DeleteAlias(ctx context.Context, tokenID domain.ID, data query.DeleteAlias) (types.Alias, error) {
 	var entity types.Alias
 
-	conn, closer, err := storage.connection(ctx)
-	if err != nil {
-		return entity, err
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
 	}
 	defer closer()
 
-	token, err := storage.exec.UserManager(ctx, conn).Token(tokenID)
-	if err != nil {
-		return entity, err
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
 	}
 	return storage.exec.AliasEditor(ctx, conn).Delete(token, data)
+}
+
+/*
+ *
+ * Target
+ *
+ */
+
+// CreateTarget TODO issue#131
+func (storage *Storage) CreateTarget(ctx context.Context, tokenID domain.ID, data query.CreateTarget) (types.Target, error) {
+	var entity types.Target
+
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
+	}
+	defer closer()
+
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
+	}
+	if _, readErr := storage.exec.LinkEditor(ctx, conn).Read(token, query.ReadLink{ID: data.LinkID}); readErr != nil {
+		return entity, readErr
+	}
+	return storage.exec.TargetEditor(ctx, conn).Create(token, data)
+}
+
+// ReadTarget TODO issue#131
+func (storage *Storage) ReadTarget(ctx context.Context, tokenID domain.ID, data query.ReadTarget) (types.Target, error) {
+	var entity types.Target
+
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
+	}
+	defer closer()
+
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
+	}
+	return storage.exec.TargetEditor(ctx, conn).Read(token, data)
+}
+
+// UpdateTarget TODO issue#131
+func (storage *Storage) UpdateTarget(ctx context.Context, tokenID domain.ID, data query.UpdateTarget) (types.Target, error) {
+	var entity types.Target
+
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
+	}
+	defer closer()
+
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
+	}
+	if _, readErr := storage.exec.LinkEditor(ctx, conn).Read(token, query.ReadLink{ID: data.LinkID}); readErr != nil {
+		return entity, readErr
+	}
+	return storage.exec.TargetEditor(ctx, conn).Update(token, data)
+}
+
+// DeleteTarget TODO issue#131
+func (storage *Storage) DeleteTarget(ctx context.Context, tokenID domain.ID, data query.DeleteTarget) (types.Target, error) {
+	var entity types.Target
+
+	conn, closer, connErr := storage.connection(ctx)
+	if connErr != nil {
+		return entity, connErr
+	}
+	defer closer()
+
+	token, authErr := storage.exec.UserManager(ctx, conn).Token(tokenID)
+	if authErr != nil {
+		return entity, authErr
+	}
+	return storage.exec.TargetEditor(ctx, conn).Delete(token, data)
 }
